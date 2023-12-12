b35f59df6cfa177711644bc5e7b2d2d5863de39c
TestLeafPages <earth@testleaf.com>
2023-12-12 13:32:52 +0530

    Add files via upload

---
+++ week1/day1/Car.java
@@ -0,0 +1,37 @@
+package week1.day1;
+
+public class Car {
+	
+	public static void main(String[] args) {
+		
+	
+		//ClassName objectName = new ClassName();
+		
+		Car obj=new Car();
+		//objectName.MethodName(related)
+		obj.soundHorn();
+		obj.applyBreak();
+		
+		System.out.println("I am Software Tester");
+	}
+	
+	public void applyBreak() {
+	
+		System.out.println("Break is applied");
+		
+	}
+	
+	
+	
+	public void soundHorn() {
+
+		System.out.println("Horn is applied");
+		
+	}
+	
+	
+
+	
+	
+
+}
---
+++ week1/day1/FirstProgram.java
@@ -0,0 +1,22 @@
+package week1.day1;
+
+public class FirstProgram {
+
+	// type -->main --press ctrl+space bar and then press enter key
+
+	
+	/*
+	 * syntax: public static void main(String[] args) {
+	 * 
+	 * }
+	 */
+	public static void main(String[] args) {
+
+	System.out.println("Welcome ");
+		
+		
+		
+	}
+	
+	
+}
