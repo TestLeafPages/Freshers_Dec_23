d47083b317a6f11362cd006db4d800c66b8570b5
TestLeafPages <earth@testleaf.com>
2023-12-14 20:32:07 +0530

    Add files via upload

---
+++ week1/day2/Mobile.java
@@ -0,0 +1,22 @@
+package week1.day2;
+
+public class Mobile {
+public static void main(String[] args) {
+	
+
+	 String mobileName="Samsung";
+	 System.out.println(mobileName);
+	 double mobilePrice=99.99;
+	 System.out.println("mobilePrice = "+mobilePrice);
+	 char firstLetter='S';
+	 boolean isDamaged=false;
+	 float frequency=87.76552f;
+	 long imeiNumber=7652372613812362317l;
+	 int mobilePiece=11221;
+	 short number=9999;
+	 byte size =6;
+	 
+	  
+	 
+}
+}
